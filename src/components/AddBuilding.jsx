3926e0f16896cf42c32b5c9345e908a3591e52fa
Jerald Wilks <jerald@wilks.pro>
2023-12-28 14:24:29 -0800

    Added styling to Key Features

--- src/components/AddBuilding.jsx
+++ src/components/AddBuilding.jsx
@@ -2,12 +2,16 @@ import React, { useState } from "react";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../providers/FirebaseProvider";
 import {
+  Box,
   Button,
+  Container,
   CssBaseline,
   FormControl,
   FormLabel,
+  Grid,
   TextField,
-  //   ThemeProvider,
+  ThemeProvider,
+  Typography,
   createTheme,
 } from "@mui/material";
 
@@ -66,103 +70,141 @@ const AddBuilding = () => {
 
   return (
     <div>
-      <CssBaseline />
-      {/* <ThemeProvider theme={theme}> */}
-      <FormControl>
-        Add a New Building:
-        <TextField
-          margin="dense"
-          type="text"
-          label="Name of Development"
-          value={name}
-          required
-          onChange={(e) => setName(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="text"
-          label="Address"
-          value={address}
-          required
-          onChange={(e) => setAddress(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="text"
-          label="City"
-          value={city}
-          required
-          onChange={(e) => setCity(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="text"
-          label="Builder"
-          value={builder}
-          onChange={(e) => setBuilder(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="number"
-          label="Number of Storeys"
-          value={storeys}
-          onChange={(e) => setStoreys(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="text"
-          label="Description of Development"
-          value={desc}
-          onChange={(e) => setDesc(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="text"
-          label="Estimated Completion"
-          value={estCompletion}
-          onChange={(e) => setEstCompletion(e.target.value)}
-        ></TextField>
-        {/* add Key Features Array */}
-        <FormLabel>Key Features:</FormLabel>
-        {keyFeatures.map((feature, index) => (
-          <React.Fragment key={"feature" + index}>
+      <ThemeProvider theme={theme}>
+        {/* <Container component="main" maxWidth="xs"> */}
+        <CssBaseline />
+        <Box
+          sx={{
+            width: 600,
+            marginTop: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <FormControl sx={{ width: 600 }}>
+            <Typography component="h1" variant="h5">
+              Add a New Building:
+            </Typography>
             <TextField
               margin="dense"
               type="text"
-              label="Feature"
-              value={feature}
-              onChange={(e) => updateFeature(index, e.target.value)}
+              label="Name of Development"
+              value={name}
+              required
+              onChange={(e) => setName(e.target.value)}
             ></TextField>
-            {keyFeatures.length > 1 && (
-              <Button onClick={() => removeFeature(index)}>-</Button>
-            )}
-          </React.Fragment>
-        ))}
-        <Button onClick={addFeature}>+</Button>
-        <TextField
-          margin="dense"
-          type="text"
-          label="Name of Contact"
-          value={contactName}
-          onChange={(e) => setContactName(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="text"
-          label="Contact Phone Number"
-          value={contactPhone}
-          onChange={(e) => setContactPhone(e.target.value)}
-        ></TextField>
-        <TextField
-          margin="dense"
-          type="text"
-          label="Contact Email"
-          value={contactEmail}
-          onChange={(e) => setContactEmail(e.target.value)}
-        ></TextField>
-        <Button onClick={submitBuilding}>Add Building</Button>
-      </FormControl>
-      {/* </ThemeProvider> */}
+            <TextField
+              margin="dense"
+              type="text"
+              label="Address"
+              value={address}
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            ></TextField>
+            <TextField
+              margin="dense"
+              type="text"
+              label="City"
+              value={city}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            ></TextField>
+            <TextField
+              margin="dense"
+              type="text"
+              label="Builder"
+              value={builder}
+              onChange={(e) => setBuilder(e.target.value)}
+            ></TextField>
+            <TextField
+              margin="dense"
+              type="number"
+              label="Number of Storeys"
+              value={storeys}
+              onChange={(e) => setStoreys(e.target.value)}
+            ></TextField>
+            <TextField
+              margin="dense"
+              type="text"
+              label="Description of Development"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            ></TextField>
+            <TextField
+              margin="dense"
+              type="text"
+              label="Estimated Completion"
+              value={estCompletion}
+              onChange={(e) => setEstCompletion(e.target.value)}
+            ></TextField>
+            <FormLabel>Key Features:</FormLabel>
+            <Grid container>
+              {keyFeatures.map((feature, index) => (
+                <React.Fragment key={"feature" + index}>
+                  <Grid item xs={10}>
+                    <TextField
+                      fullWidth
+                      margin="dense"
+                      type="text"
+                      label="Feature"
+                      value={feature}
+                      onChange={(e) => updateFeature(index, e.target.value)}
+                    />
+                  </Grid>
+                  {keyFeatures.length > 1 && (
+                    <Grid item xs={2}>
+                      <Button
+                        sx={{ m: 2 }}
+                        onClick={() => removeFeature(index)}
+                        color="error"
+                        variant="outlined"
+                      >
+                        -
+                      </Button>
+                    </Grid>
+                  )}
+                </React.Fragment>
+              ))}
+            </Grid>
+            <Grid container justifyContent={"center"}>
+              <Grid item xs={6}>
+                <Button
+                  sx={{ m: 1 }}
+                  fullWidth
+                  onClick={addFeature}
+                  variant="outlined"
+                >
+                  +
+                </Button>
+              </Grid>
+            </Grid>
+            <TextField
+              margin="dense"
+              type="text"
+              label="Name of Contact"
+              value={contactName}
+              onChange={(e) => setContactName(e.target.value)}
+            ></TextField>
+            <TextField
+              margin="dense"
+              type="text"
+              label="Contact Phone Number"
+              value={contactPhone}
+              onChange={(e) => setContactPhone(e.target.value)}
+            ></TextField>
+            <TextField
+              margin="dense"
+              type="text"
+              label="Contact Email"
+              value={contactEmail}
+              onChange={(e) => setContactEmail(e.target.value)}
+            ></TextField>
+            <Button onClick={submitBuilding}>Add Building</Button>
+          </FormControl>
+        </Box>
+        {/* </Container> */}
+      </ThemeProvider>
     </div>
   );
 };
